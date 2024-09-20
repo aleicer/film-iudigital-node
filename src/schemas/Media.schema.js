4bfc3531dc7d1a7a3b36971e0a330ea0c0e08a19
Samuel Rosero <saferoxx215@gmail.com>
2024-09-20 10:37:59 -0500

    change scheema

--- src/schemas/Media.schema.js
+++ src/schemas/Media.schema.js
@@ -1,19 +1,22 @@
-import { Schema, model } from 'mongoose'
+import { Schema, model } from 'mongoose';
 
-const MediaSchema = new Schema({
-  _id: { type: Schema.Types.ObjectId, auto: true },
-  serial: { type: String, required: true, unique: true },
-  title: { type: String, required: true },
-  synopsis: { type: String, required: true },
-  url: { type: String, required: true, unique: true },
-  coverImage: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now },
-  releaseYear: { type: Date, required: true },
-  genre: { type: Schema.Types.ObjectId, required: false, ref: 'Genre' },
-  director: { type: Schema.Types.ObjectId, required: false, ref: 'Director' },
-  producer: { type: Schema.Types.ObjectId, required: false, ref: 'Producer' },
-  type: { type: Schema.Types.ObjectId, required: false, ref: 'Type' },
-}, { timestamps: true })
+const MediaSchema = new Schema(
+  {
+    _id: { type: Schema.Types.ObjectId, auto: true },
+    serial: { type: String, required: true, unique: true },
+    title: { type: String, required: true },
+    synopsis: { type: String, required: true },
+    url: { type: String, required: true, unique: false },
+    coverImage: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+    releaseYear: { type: Date, required: true },
+    genre: { type: Schema.Types.ObjectId, required: false, ref: 'Genre' },
+    director: { type: Schema.Types.ObjectId, required: false, ref: 'Director' },
+    producer: { type: Schema.Types.ObjectId, required: false, ref: 'Producer' },
+    type: { type: Schema.Types.ObjectId, required: false, ref: 'Type' },
+  },
+  { timestamps: true }
+);
 
-export const MediaModel = model('Media', MediaSchema)
+export const MediaModel = model('Media', MediaSchema);
--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ import { directorsController } from './controllers/directors.controller.js';
 
 await getConnectionMongoDB();
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
 
 app.use(express.json());
 app.use(cors());
